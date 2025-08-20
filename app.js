3f09c6a383f76238513208c220be62b46ee6cd1e
Anand Duggirala <dvpnand@hotmail.com>
2025-08-20 23:21:56 +1200

    yes 	modified:   app.js 	new file:   images/Azure-Blue-Sky-Gradient.png 	new file:   images/Vibrant-Blue-Sky-Gradient.png 	modified:   index.html 	modified:   style.css

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-part2");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
+
+
+const menuIcon = document.querySelector(".nav-part2 i");
+const menuLinks = document.querySelector(".menu-links");
+
+menuIcon.addEventListener("click", () => {
+  menuLinks.classList.toggle("active");
+});
+
+*/
+
+const hamburgerToggle = document.querySelector('.hamburger-toggle');
+const menuLinks = document.querySelector('.menu-links');
+
+hamburgerToggle.addEventListener('click', function() {
+    // Toggle menu visibility
+    menuLinks.classList.toggle('active');
+    
+    // Change icon
+    if (menuLinks.classList.contains('active')) {
+        hamburgerToggle.className = 'ri-close-line hamburger-toggle active';
+    } else {
+        hamburgerToggle.className = 'ri-menu-3-fill hamburger-toggle';
+    }
+});
